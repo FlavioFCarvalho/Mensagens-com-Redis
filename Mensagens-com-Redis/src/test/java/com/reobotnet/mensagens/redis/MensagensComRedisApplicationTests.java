021616cb23dc1e3ef8cf2d75595f6a003ecabd40
FlavioFCarvalho <flaviocfr@gmail.com>
2021-08-26 18:23:02 -0300

    Mensagens com Redis

---
+++ Mensagens-com-Redis/src/test/java/com/reobotnet/mensagens/redis/MensagensComRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reobotnet.mensagens.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensagensComRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
